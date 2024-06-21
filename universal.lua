33d6282ebb0b66a555eb5c928cd9e90680928823
DRKscripts <168048927+DRKscripts@users.noreply.github.com>
2024-06-21 16:25:56 -0300

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,31 +1,31 @@
 local gameid = game.PlaceId
 if(gameid == 286090429) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Arsenal/main/Arsenal-Script.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Arsenal/main/Arsenal-Script.lua"))(), true))()
 end
 
 local gameid = game.PlaceId
 if(gameid == 155615604) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Prison/main/DRKHUBprisonlife.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Prison/main/DRKHUBprisonlife.lua"))(), true))()
 end
 
 local gameid = game.PlaceId
 if(gameid == 606849621) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Jailbreak/main/jailbreak.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Jailbreak/main/jailbreak.lua"))(), true))()
 end
 
 local gameid = game.PlaceId
 if(gameid == 893973440) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Murder/main/DRK.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Murder/main/DRK.lua"))(), true))()
 end
 
 local gameid = game.PlaceId
 if(gameid == 142823291) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Murder/main/DRK.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Murder/main/DRK.lua"))(), true))()
 end
 
 local gameid = game.PlaceId
 if(gameid == 13864661000) then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Breaking2/main/BreakIn2.lua"))(), true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRKscripts/Breaking2/main/BreakIn2.lua"))(), true))()
 end
 
 -- Anti Afk
